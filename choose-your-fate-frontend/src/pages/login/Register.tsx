e1fcc994ba343c2048dea34f8d74b90a773ee729
Mikkel Olsen <molybdenum93@gmail.com>
2026-05-30 19:08:21 +0200

    fixed imports

--- choose-your-fate-frontend/src/pages/login/Register.tsx
+++ choose-your-fate-frontend/src/pages/login/Register.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { register } from "../../api/authApi"
-import "./login.css"
+import "./Login.css"
 
 export default function Register() {
     const navigate = useNavigate();
